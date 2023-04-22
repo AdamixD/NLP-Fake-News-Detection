ec7b1b7861da65ebf5b1cdae5cae1434964b8f23
Maciej Lodzinski <mf.lodzinski@gmail.com>
2023-04-22 22:09:11 +0200

    f1 score added to fit function

--- training/model.py
+++ training/model.py
@@ -2,6 +2,7 @@ from tensorflow.keras.optimizers import Adam
 import tensorflow.keras as keras
 from transformers import TFAutoModelForSequenceClassification
 from transformers import AutoTokenizer
+from keras import backend as K
 import pyarrow as pa
 from datasets import Dataset, DatasetDict
 import pandas as pd
@@ -59,10 +60,27 @@ class Model:
     def compile(self):
         loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
         metrics = keras.metrics.SparseCategoricalAccuracy('accuracy')
-        self.model.compile(optimizer=Adam(3e-5), loss=loss, metrics=[metrics])
+        self.model.compile(optimizer=Adam(3e-5), loss=loss, metrics=[metrics, self.f1_m])
 
     def fit(self, train_data, validation_data, epochs=3):
         return self.model.fit(train_data, epochs=epochs, validation_data=validation_data)
+    
+    def recall_m(self, y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall
+
+    def precision_m(self, y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+    
+    def f1_m(self, y_true, y_pred):
+        precision = self.precision_m(y_true, y_pred)
+        recall = self.recall_m(y_true, y_pred)
+        return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
     def evaluate(self, dataset):
         return self.model.evaluate(dataset)
